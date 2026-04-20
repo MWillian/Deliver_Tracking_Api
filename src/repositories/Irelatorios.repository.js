5e3ac1b761aaec9faef475493ababb03fe8d50bb
MWillian <Matheuswno02@gmail.com>
2026-04-19 22:34:59 -0300

    feat 02
    - Adicionadas dependencias iniciais do prisma para gerenciamento do banco de dados.
    - Criação da interface IRelatoriosRepository  para definir os contratos esperados da implementação do repositório.

---
+++ src/repositories/Irelatorios.repository.js
@@ -0,0 +1,8 @@
+/**
+ * Contrato esperado para implementacoes de RelatoriosRepository.
+ *
+ * @typedef {Object} IRelatoriosRepository
+ * @property {() => Promise<Array<Object>>} contarEntregasPorStatus
+ * @property {() => Promise<Array<Object>>} listarMotoristasAtivosComEntregasAbertas
+ */
+export {};
