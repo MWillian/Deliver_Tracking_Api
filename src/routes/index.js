75d61449d645def11fad7c11011548cdd750fc50
MWillian <matheuswno02@gmail.com>
2026-05-27 12:59:28 -0300

    Adiciona  autenticação e autorização com JWT direto nas rotas.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,14 @@ import entregasRouter from './entregas.router.js';
 import motoristasRouter from './motoristas.router.js'
 import relatoriosRouter from './relatorios.router.js';
 import authRouter from './auth.router.js';
+import { autenticar } from '../middlewares/autenticacao.middleware.js';
 
 export const router = Router();
 
 router.use('/auth', authRouter);
+
+router.use(autenticar);
+
 router.use('/entregas', entregasRouter);
 router.use('/motoristas', motoristasRouter);
 router.use('/relatorios', relatoriosRouter);
