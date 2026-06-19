8a5004c658d3fe192d92547d24d5eef29895f45e
MWillian <matheuswno02@gmail.com>
2026-06-19 18:03:35 -0300

    Refatoração integral do front-end para renderização via EJS padrão. Ajuste do middleware de autenticação para armazenar o jwt em localStorage e Cookie, para poder manejar o token nas requisições realizadas a partir das views EJS.

--- src/controllers/painel/motoristas.controller.js
+++ src/controllers/painel/motoristas.controller.js
@@ -7,11 +7,26 @@ export class PainelMotoristasController {
         this.exibirFormularioCriacao = this.exibirFormularioCriacao.bind(this);
         this.criar = this.criar.bind(this);
         this.exibirDetalhe = this.exibirDetalhe.bind(this);
+        this.inativar = this.inativar.bind(this);
     }
 
     async listarTodos(req, res, next) {
         try {
-            res.render('motoristas/index');
+            const { status, sucesso, erro } = req.query;
+            let motoristas;
+
+            if (status) {
+                motoristas = await this.service.listarComFiltros(status);
+            } else {
+                motoristas = await this.service.listarTodos();
+            }
+
+            res.render('motoristas/index', {
+                motoristas,
+                sucesso,
+                erro,
+                statusSelecionado: status || ''
+            });
         } catch (err) {
             next(err);
         }
@@ -49,9 +64,32 @@ export class PainelMotoristasController {
 
     async exibirDetalhe(req, res, next) {
         try {
-            res.render('motoristas/detalhe');
+            const id = Number(req.params.id);
+            const { status, sucesso } = req.query;
+
+            if (!Number.isInteger(id) || id < 1) {
+                throw new AppError('ID inválido.', 404);
+            }
+
+            const motorista = await this.service.listarPorId(id);
+            const entregas = await this.service.listarEntregasPorId(id, { status });
+
+            res.render('motoristas/detalhe', {
+                motorista,
+                entregas,
+                statusSelecionado: status || '',
+                sucesso
+            });
         } catch (err) {
             next(err);
+        }
+    }
+    async inativar(req, res, next) {
+        try {
+            const motoristaCancelado = await this.service.inativar(Number(req.params.id));
+            res.redirect('/painel/motoristas?sucesso=Motorista inativado com sucesso');
+        } catch (err) {
+            res.redirect(`/painel/motoristas?erro=${encodeURIComponent(err.message)}`);
         }
     }
 }
